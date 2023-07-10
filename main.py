34a209c0437c04a578d6009d01ea41da0e6eb29b
vaibhav3105 <jainvaibhav312003@gmail.com>
2023-07-10 14:53:30 +0530

    commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import cv2
+from ultralytics import YOLO
+
+
+
+model = YOLO('best.pt')
+
+# Open the video file
+video_path = "SaveTube.io-License Plate Detection Test-(1080p).mp4"
+cap = cv2.VideoCapture(video_path)
+
+# Loop through the video frames
+while cap.isOpened():
+    # Read a frame from the video
+    success, frame = cap.read()
+
+    if success:
+        # Run YOLOv8 inference on the frame
+        results = model(frame)
+        print(results[0])
+        # Visualize the results on the frame
+        annotated_frame = results[0].plot()
+
+        # Display the annotated frame
+        cv2.imshow("YOLOv8 Inference", annotated_frame)
+
+        # Break the loop if 'q' is pressed
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    else:
+        # Break the loop if the end of the video is reached
+        break
+
+# Release the video capture object and close the display window
+cap.release()
+cv2.destroyAllWindows()
